0e23883a941910945c1129860811fbd6410cd443
Camille Le <lecamille@gmail.com>
2020-10-04 11:41:03 -0700

    10/04 Update README with Flask information

--- kindle_quotations/routes.py
+++ kindle_quotations/routes.py
@@ -1,12 +1,3 @@
-""" 
-So how exactly does Flask handle uploads? Well it will store them in the 
-webserver’s memory if the files are reasonable small otherwise in a temporary 
-location (as returned by tempfile.gettempdir()). But how do you specify 
-the maximum file size after which an upload is aborted? By default 
-Flask will happily accept file uploads to an unlimited amount of memory, 
-but you can limit that by setting the MAX_CONTENT_LENGTH config key:
-"""
-
 import os 
 import random
 from flask import render_template, url_for, flash, redirect, request, send_from_directory 
@@ -22,7 +13,6 @@ ALLOWED_EXTENSIONS = {'xml'}
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-# limit upload size upto 8mb
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
 
 
@@ -30,12 +20,10 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
 @app.route("/")
 @app.route("/home")
 def home():    
     return render_template('home.html') 
-
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload(): 
@@ -54,22 +42,15 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))       
-            
-            # process file information 
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename) 
-            process(filepath) 
-            
+            process_file(filename)            
             return redirect(url_for('uploaded_file', filename=filename))
     return render_template('upload.html')    
     
 
-#def process_file(path, filename):
- #   process(path) 
+def process_file(filename):
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename) 
+    process(filepath) 
     
-
- #   remove_watermark(path, filename)    
- # output_stream = open(app.config['DOWNLOAD_FOLDER'] + filename, 'wb')
-
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):    
