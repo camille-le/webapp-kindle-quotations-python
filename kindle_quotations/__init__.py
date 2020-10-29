83a5d89778c6fbf9a4d2164d54e595d61d979933
Camille <31285549+camille-le@users.noreply.github.com>
2020-10-29 13:11:44 -0700

    Update __init__.py

--- kindle_quotations/__init__.py
+++ kindle_quotations/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f1c6cea865888e5ee83009b4f20d19a6'
+app.config['SECRET_KEY'] = '[password]'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
 db = SQLAlchemy(app)
 
