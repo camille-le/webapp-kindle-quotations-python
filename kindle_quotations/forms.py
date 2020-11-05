8c1457073f7bc8993efc9803466d9eda65b2bc8e
Camille <lecamille@gmail.com>
2020-11-04 23:10:47 -0800

    Add XML processing feature

--- kindle_quotations/forms.py
+++ kindle_quotations/forms.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired 
+from flask_wtf.file import FileField, FileRequired, FileAllowed 
 from wtforms.validators import DataRequired 
 from wtforms import SubmitField
 
 
 class HTMLtoCSVForm(FlaskForm):
-    filename = FileField(validators=[FileRequired()]) 
-    submit = SubmitField('Download CSV')
+    filename = FileField(validators=[FileRequired(), FileAllowed(['xml', 'html'], "HTML/XML Only!")]) 
+    submit1 = SubmitField('Download CSV')
+    
+class HTMLtoJSONForm(FlaskForm):    
+    filename = FileField(validators=[FileRequired(), FileAllowed(['xml', 'html'], "HTML/XML Only!")]) 
+    submit2 = SubmitField('Download JSON')
+
 
 
--- kindle_quotations/routes.py
+++ kindle_quotations/routes.py
@@ -4,29 +4,46 @@ from flask import render_template, url_for, flash, redirect, request, send_from_
 from werkzeug.utils import secure_filename
 from kindle_quotations import app 
 from kindle_quotations.models import process
-from kindle_quotations.forms import HTMLtoCSVForm
+from kindle_quotations.forms import HTMLtoCSVForm, HTMLtoJSONForm
 import io; io.StringIO()
+import json
 
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
-ALLOWED_EXTENSIONS = {'html', 'xml'}
-
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+HOME = 'home.html'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 # 1 MB
 
 
 @app.route("/")
-@app.route("/home", methods=['GET', 'POST'])
 def home(): 
-    form = HTMLtoCSVForm() 
-    if form.validate_on_submit():         
-        upload_file = form.filename.data # FileStore object         
+    csv_form = HTMLtoCSVForm() 
+    json_form = HTMLtoJSONForm() 
+    return render_template(HOME, csv_form=csv_form, json_form=json_form)
+
+
+@app.route('/csv', methods=['POST'])
+def csv(): 
+    csv_form = HTMLtoCSVForm() 
+    json_form = HTMLtoJSONForm() 
+    if csv_form.validate_on_submit(): 
+        upload_file = csv_form.filename.data # FileStore object         
         fname = secure_filename(upload_file.filename) 
         upload_file.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
-        if form.submit.label.text == "Download CSV":   
-            return download_csv(fname)                              
-    return render_template('home.html', form=form)
+        return download_csv(fname)      
+    return render_template(HOME, csv_form=csv_form, json_form=json_form)                        
+    
+@app.route('/htmljson', methods=['POST'])
+def htmljson(): 
+    csv_form = HTMLtoCSVForm() 
+    json_form = HTMLtoJSONForm() 
+    if json_form.validate_on_submit(): 
+        upload_file = csv_form.filename.data # FileStore object         
+        fname = secure_filename(upload_file.filename) 
+        upload_file.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+        return download_json(fname)
+    return render_template(HOME, csv_form=csv_form, json_form=json_form)    
 
 
 @app.route('/uploads/<filename>')
@@ -39,9 +56,17 @@ def contact():
     return render_template('contact.html')
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+def download_json(filename): 
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)     
+    data = process(filepath, 'json') # returns a list of dict      
+    content_disposition = "attachment; filename="
+    content_disposition += filename + ".json"
+    response = make_response(json.dumps(data, indent=2)) 
+    response.headers["Content-Disposition"] = content_disposition
+    response.mimetype = 'application/json'
+    os.remove(filepath)        
+    return response
+        
 
 def download_csv(filename):
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)     
@@ -57,4 +82,5 @@ def download_csv(filename):
     response.headers["Content-type"] = "text/csv"
     response.mimetype='text/csv'
     return response        
+
 
